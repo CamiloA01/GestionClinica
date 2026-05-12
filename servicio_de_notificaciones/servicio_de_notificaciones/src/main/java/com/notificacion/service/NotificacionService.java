721f888a8f929705063b38d2e2dff3950273519a
VI-Alumno <VI-Alumno@duoc.acad>
2026-05-12 11:20:33 -0400

    avance de notificacion en service

--- servicio_de_notificaciones/servicio_de_notificaciones/src/main/java/com/notificacion/service/NotificacionService.java
+++ servicio_de_notificaciones/servicio_de_notificaciones/src/main/java/com/notificacion/service/NotificacionService.java
@@ -1,5 +1,43 @@
 package com.notificacion.service;
 
+import java.util.List;
+
+import com.notificacion.model.Notificacion;
+import com.notificacion.model.dto.NotificacionResponseDTO;
+import com.notificacion.model.dto.notificacionRequestDTO;
+
 public class NotificacionService {
+
+    private final NotificacionRepository notificacionRepository;
+
+    //MAPEO PRIVADO ENTRE ENTIDAD Y ResponseDTO
+    private NotificacionResponseDTO mapToDTO(Notificacion notificacion){
+        return new NotificacionResponseDTO(
+            notificacion.getId(),
+            notificacion.getIdUsuario(),
+            notificacion.getTitulo(),
+            notificacion.getMensaje(),
+            notificacion.getTipo(),
+            notificacion.getEstado(),
+            notificacion.getFechaEnvio(),
+            notificacion.getFechaLectura()
+        );
+    }
+
+    //OBTENER TODAS LAS NOTIFICAIONES
+    public List<NotificacionResponseDTO> obtenerTodas(){
+        return notificacionRepository.findAll()
+        .stream().map(this::mapToDTO).collect(collectors.toList());
+    }
+
+    //OBTENER POR ID
+    public Optional<NotificacionResponseDTO> obtenerPorId(Long id){
+        return notificacionRepository.findById(id).map(this::mapToDTO);
+    }
+
+    //GUARDAR NOTIFICACIÓN
+    public NotificacionResponseDTO guardar (notificacionRequestDTO dto){
+        Notificacion notificacion = notificacionRepository.findById(dto.)
+    }
 
 }
