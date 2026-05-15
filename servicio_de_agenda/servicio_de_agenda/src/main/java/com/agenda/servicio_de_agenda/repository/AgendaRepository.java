dbcaed36b398395eef6023e87d5bd4575d7e3bca
Abrahan Alvarado <ab.alvarado@duocuc.cl>
2026-05-15 12:25:32 -0400

    ms notificaciones terminado y ms agenda avanzado

--- servicio_de_agenda/servicio_de_agenda/src/main/java/com/agenda/servicio_de_agenda/repository/AgendaRepository.java
+++ servicio_de_agenda/servicio_de_agenda/src/main/java/com/agenda/servicio_de_agenda/repository/AgendaRepository.java
@@ -7,5 +7,5 @@ import com.agenda.servicio_de_agenda.model.Agenda;
 public interface AgendaRepository extends JpaRepository<Agenda, Long> {
 
     List<Agenda> findByDuracionCita(Integer duracionCita);
-
+a
 }
