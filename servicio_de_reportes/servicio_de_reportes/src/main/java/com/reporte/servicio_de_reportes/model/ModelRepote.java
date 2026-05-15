fbd4d1ee2bd52627a9ca4f0a5ec57baa258ed9c6
VI-Alumno <VI-Alumno@duoc.acad>
2026-05-15 13:52:51 -0400

    hola

---
+++ servicio_de_reportes/servicio_de_reportes/src/main/java/com/reporte/servicio_de_reportes/model/ModelRepote.java
@@ -0,0 +1,35 @@
+package com.reporte.servicio_de_reportes.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "reportes")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class ModelRepote {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombreReporte;
+
+    @Column(nullable = false)
+    private String tipo;
+
+    @Column(nullable = false)
+    private String requestdBy;
+
+    @Column(nullable = false)
+    private LocalDate generatedAt;
+
+
+}
