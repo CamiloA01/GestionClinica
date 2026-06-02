2b974d4fe8d32479b78d0d50824f106c4779cdc0
VI-Alumno <VI-Alumno@duoc.acad>
2026-06-02 12:23:07 -0400

    hateoas

--- servicio_de_usuario/servicio_de_usuario/src/main/java/com/example/servicio_de_Ususarios/assembler/ClinicaModelAssembler.java
+++ servicio_de_usuario/servicio_de_usuario/src/main/java/com/example/servicio_de_Ususarios/assembler/ClinicaModelAssembler.java
@@ -1,5 +1,20 @@
 package com.example.servicio_de_Ususarios.assembler;
 
-public class ClinicaModelAssembler {
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;    
+
+import com.example.servicio_de_Ususarios.controller.*;
+import com.example.servicio_de_Ususarios.model.*;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ClinicaModelAssembler implements RepresentationModelAssembler<Usuario, EntityModel<Usuario> {
+    @Override
+    public EntityModel<Usuario> toModel(Usuario usuario){
+        return EntityModel.of(usuario,
+                linkTo(methodOn(usuarioController.class).getAllUsuarios()).withRel("carreras"));
+        
+    }
 
 }
