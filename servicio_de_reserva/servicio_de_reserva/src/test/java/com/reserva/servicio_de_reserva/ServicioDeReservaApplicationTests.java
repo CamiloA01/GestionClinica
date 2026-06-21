f7a7ca7c820419721839012887d222a4bfc18001
Oscar Carrasco <os.carrascco@duocuc.cl>
2026-06-21 16:02:42 -0400

    Si

--- servicio_de_reserva/servicio_de_reserva/src/test/java/com/reserva/servicio_de_reserva/ServicioDeReservaApplicationTests.java
+++ servicio_de_reserva/servicio_de_reserva/src/test/java/com/reserva/servicio_de_reserva/ServicioDeReservaApplicationTests.java
@@ -1,13 +1,3 @@
 package com.reserva.servicio_de_reserva;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ServicioDeReservaApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
